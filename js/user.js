121a21a7d55481db11c561359437e8da395ab0e0
oak11 <oaks9599@gmail.com>
2016-01-04 18:32:06 +0530

    changes

--- js/user.js
+++ js/user.js
@@ -1,6 +1,10 @@
 function main() {
-  var db = new Kinto({remote: "https://kinto.dev.mozaws.net/v1"});
-  var tasks = db.collection("users");
+  var db = new Kinto({
+    remote: "https://kinto.dev.mozaws.net/v1/",
+    headers: {Authorization: "Basic " + btoa("user:pass")}
+  });
+  var tasks = db.collection("user");
+
 
   document.getElementById("form")
     .addEventListener("submit", function(event) {
@@ -8,18 +12,118 @@ function main() {
       tasks.create({
         title: event.target.title.value,
         url: event.target.url.value,
-        done: false
+        
       })
       .then(function(res) {
         event.target.title.value = "";
-        event.target.url.value = "";
+        event.target.url.value = ""
         event.target.title.focus();
       })
+      .then(render)
       .catch(function(err) {
         console.error(err);
       });
     });
 
+  document.getElementById("clearCompleted")
+    .addEventListener("click", function(event) {
+      event.preventDefault();
+      tasks.list()
+        .then(function(res) {
+          var completed = res.data.filter(function(task) {
+            return task.done;
+          });
+          return Promise.all(completed.map(function(task) {
+            return tasks.delete(task.id);
+          }));
+        })
+        .then(render)
+        .catch(function(err) {
+          console.error(err);
+        });
+    });
+
+  function doSync() {
+    return tasks.sync().catch(function(err) {
+      if (err.message.contains("flushed")) {
+        console.warn("Flushed server detected, marking local data for reupload.");
+        return tasks.resetSyncStatus()
+          .then(function() {
+            return tasks.sync();
+          });
+      }
+      throw err;
+    });
+  }
+
+  function handleConflicts(conflicts) {
+    return Promise.all(conflicts.map(function(conflict) {
+      return tasks.resolve(conflict, conflict.remote);
+    }))
+      .then(doSync);
+  }
+
+  document.getElementById("sync")
+    .addEventListener("click", function(event) {
+      event.preventDefault();
+      doSync()
+        .then(function(res) {
+          document.getElementById("results").value = JSON.stringify(res, null, 2);
+          if (res.conflicts.length) {
+            return handleConflicts(res.conflicts);
+          }
+          return res;
+        })
+        .then(render)
+        .catch(function(err) {
+          console.error(err);
+        });
+    });
+
+  function renderTask(task) {
+    var tpl = document.getElementById("task-tpl");
+    var li = tpl.content.cloneNode(true);
+    li.querySelector(".title").textContent = task.title;
+    li.querySelector(".uuid").textContent = task.id;
+    li.querySelector(".url").textContent = task.url;
+    // retrieve a reference to the checkbox element
+    var checkbox = li.querySelector(".done");
+    // initialize it with task status
+    checkbox.checked = task.done;
+    // listen to cliecks
+    checkbox.addEventListener("click", function(event) {
+      // prevent the click to actually toggle the checkbox
+      event.preventDefault();
+      // invert the task status
+      task.done = !task.done;
+      // update task status
+      tasks.update(task)
+        .then(render)
+        .catch(function(err) {
+          console.error(err);
+        });
+    });
+    return li;
+  }
+
+  function renderTasks(tasks) {
+    var ul = document.getElementById("tasks");
+    ul.innerHTML = "";
+    tasks.forEach(function(task) {
+      ul.appendChild(renderTask(task));
+    });
+  }
+
+  function render() {
+    tasks.list().then(function(res) {
+      renderTasks(res.data);
+    }).catch(function(err) {
+      console.error(err);
+    });
+  }
+
+  render();
 }
+
 
 window.addEventListener("DOMContentLoaded", main);
