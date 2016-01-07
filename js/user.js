1f6ddda6d4ad5528cd6607eb801478c95e45a5e7
oak11 <oaks9599@gmail.com>
2016-01-07 11:57:06 +0530

    trial 9

--- js/user.js
+++ js/user.js
@@ -1,131 +1,84 @@
-function main() {
-  var db = new Kinto({
-//--location of form, whats inside remote
-    remote: sessionStorage.getItem("kinto_server"),
-    headers: {Authorization: "Basic " + btoa("user:pass")}
-  });
-  var tasks = db.collection("user");
+function authenticate(){
+    var storageServer = "https://kinto.dev.mozaws.net/v1";
+    sessionStorage.setItem('kinto_server',storageServer);
+   function loginURI(website) {
+      var currentWebsite = website.replace(/#.*/, '');
+      sessionStorage.setItem('origin', currentWebsite.slice(0,32));
+      var login = storageServer.replace("v1", "v1/fxa-oauth/login?redirect=");
+      var redirect = encodeURIComponent(currentWebsite + '#fxa:');
+      return login + redirect;
+      }
+   sessionStorage.setItem('origin', )
+   var uri = loginURI(window.location.href);
+   window.location = uri;
 
+ }
+// document.getElementById('login').setAttribute('href', uri);
+//  if (window.location.hash.slice(1))
+var hash = window.location.hash;
+var headers = {};
 
-  document.getElementById("form")
-    .addEventListener("submit", function(event) {
-      event.preventDefault();
-      tasks.create({
-        title: event.target.title.value,
-        url: event.target.url.value,
+if (hash.indexOf('#fxa:') == 0) {
+  // We've got a token
+  storageServer = sessionStorage.getItem('kinto_server');
+  headers['Authorization'] = 'Bearer ' + hash.slice(5);
+  var result = fetch(storageServer + '/', {headers: headers})
+    .then(function (result) {
+      return result.json();
+    })
+    .then(function (result) {
+      return result.user.id;
+    })
+    .then(function(user_id) {
+      console.log(user_id);
+      $("#user_id").text(user_id);
+      sessionStorage.setItem('user_id',user_id);
+      var db = new Kinto({
+    //--location of form, whats inside remote
+        remote: sessionStorage.getItem("kinto_server"),
+        headers: {Authorization: "Basic " + btoa("user:pass")}
+      });
+      var users = db.collection("user");
+
+      function renderUser(user) {
+         var li = document.createElement("li");
+         li.classList.add("list-group-item");
+         li.innerHTML = user.title;
+         return li;
+        }
+
+     function renderTasks(tasks) {
+        var ul = document.getElementById("tasks");
+        ul.innerHTML = "";
+        tasks.forEach(function(task) {
+          ul.appendChild(renderTask(task));
+          });
+        }
+
+      function render() {
+        tasks.list().then(function(res) {
+          renderTasks(res.data);
+        })
+        .catch(function(err) {
+          console.error(err);
+        });
+      }
+    //  if (users.forEach(users.user_id) == user_id){
+
+          //list users
+      //}
+
+      users.create({
+        url: sessionStorage.getItem('kinto_server'),
         user_id: sessionStorage.getItem('user_id')
        })
-      .then(function(res) {
-        event.target.title.value = "";
-        event.target.url.value = ""
-        event.target.title.focus();
-      })
       .then(render)
+      .then(newLocation)
       .catch(function(err) {
         console.error(err);
       });
     });
-
-  document.getElementById("clearCompleted")
-    .addEventListener("click", function(event) {
-      event.preventDefault();
-      tasks.list()
-        .then(function(res) {
-          var completed = res.data.filter(function(task) {
-            return task.done;
-          });
-          return Promise.all(completed.map(function(task) {
-            return tasks.delete(task.id);
-          }));
-        })
-        .then(render)
-        .catch(function(err) {
-          console.error(err);
-        });
-    });
-
-  function doSync() {
-    return tasks.sync().catch(function(err) {
-      if (err.message.contains("flushed")) {
-        console.warn("Flushed server detected, marking local data for reupload.");
-        return tasks.resetSyncStatus()
-          .then(function() {
-            return tasks.sync();
-          });
-      }
-      throw err;
-    });
   }
-
-  function handleConflicts(conflicts) {
-    return Promise.all(conflicts.map(function(conflict) {
-      return tasks.resolve(conflict, conflict.remote);
-    }))
-      .then(doSync);
-  }
-
-  document.getElementById("sync")
-    .addEventListener("click", function(event) {
-      event.preventDefault();
-      doSync()
-        .then(function(res) {
-          document.getElementById("results").value = JSON.stringify(res, null, 2);
-          if (res.conflicts.length) {
-            return handleConflicts(res.conflicts);
-          }
-          return res;
-        })
-        .then(render)
-        .catch(function(err) {
-          console.error(err);
-        });
-    });
-
-  function renderTask(task) {
-    var tpl = document.getElementById("task-tpl");
-    var li = tpl.content.cloneNode(true);
-    li.querySelector(".title").textContent = task.title;
-    li.querySelector(".uuid").textContent = task.id;
-    li.querySelector(".url").textContent = task.url;
-    li.querySelector(".user_id").textContent = task.user_id;
-    // retrieve a reference to the checkbox element
-    var checkbox = li.querySelector(".done");
-    // initialize it with task status
-    checkbox.checked = task.done;
-    // listen to cliecks
-    checkbox.addEventListener("click", function(event) {
-      // prevent the click to actually toggle the checkbox
-      event.preventDefault();
-      // invert the task status
-      task.done = !task.done;
-      // update task status
-      tasks.update(task)
-        .then(render)
-        .catch(function(err) {
-          console.error(err);
-        });
-    });
-    return li;
-  }
-
-  function renderTasks(tasks) {
-    var ul = document.getElementById("tasks");
-    ul.innerHTML = "";
-    tasks.forEach(function(task) {
-      ul.appendChild(renderTask(task));
-    });
-  }
-
-  function render() {
-    tasks.list().then(function(res) {
-      renderTasks(res.data);
-    }).catch(function(err) {
-      console.error(err);
-    });
-  }
-
-  render();
-}
-
-
-window.addEventListener("DOMContentLoaded", main);
+function newLocation() {
+ window.location=sessionStorage.getItem('origin')+landing_page.html;
+ }
