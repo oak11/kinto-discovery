6bee4c5fcf297fdd1371e16c48d2504d0cad19cd
oak11 <oaks9599@gmail.com>
2016-01-11 00:14:54 +0530

    url has user_id

--- js/user.js
+++ js/user.js
@@ -57,7 +57,7 @@ if (hash.indexOf('#fxa:') == 0) {
       'Authorization': authorization,
     };
 
-    var url = storageServer+'/buckets/'+ bucket +'/collections/'+ collection + '/records';
+    var url = storageServer+'/buckets/'+ bucket +'/collections/'+ collection + '/records?user_id=<' + user_id + '>';
     fetch(url, {headers: headers})
    .then(function (response) {
      console.log(response);
