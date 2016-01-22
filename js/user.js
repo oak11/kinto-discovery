a7c2be08a3c4fcade9404bdcc1c113f1a291e7ab
oak11 <oaks9599@gmail.com>
2016-01-22 20:12:42 +0530

    link storage

--- js/user.js
+++ js/user.js
@@ -1,4 +1,6 @@
+var central_repository_server = "https://kinto.dev.mozaws.net/v1";
 function authenticate(){
+
     var storageServer = document.getElementById("kinto_server").value;
     if (storageServer == ''){
       storageServer = "https://kinto.dev.mozaws.net/v1"  //default
@@ -7,7 +9,7 @@ function authenticate(){
    function loginURI(website) {
       var currentWebsite = website.replace(/#.*/, '');
       sessionStorage.setItem('origin', currentWebsite.slice(0,38)); //find a better way- more generalized
-      var login = storageServer.replace("v1", "v1/fxa-oauth/login?redirect=");
+      var login = central_repository_server.replace("v1", "v1/fxa-oauth/login?redirect=");
       var redirect = encodeURIComponent(sessionStorage.getItem('origin') + '/landing_page.html' + '#fxa:');
       return login + redirect;
       }
@@ -24,7 +26,7 @@ if (hash.indexOf('#fxa:') == 0) {
   // We've got a token
   storageServer = sessionStorage.getItem('kinto_server');
   headers['Authorization'] = 'Bearer ' + hash.slice(5);
-  var result = fetch(storageServer + '/', {headers: headers})
+  var result = fetch(central_repository_server + '/', {headers: headers})
     .then(function (result) {
       return result.json();
     })
@@ -41,7 +43,7 @@ if (hash.indexOf('#fxa:') == 0) {
       var user_record_id = uuid.v4({random: input});
 
     //  sessionStorage.setItem('user_id','1234545678yoo ');
-    var url = storageServer+'/buckets/'+ bucket +'/collections/'+ collection + '/records/'+ user_record_id;
+    var url = central_repository_server +'/buckets/'+ bucket +'/collections/'+ collection + '/records/'+ user_record_id;
     //above although filter for unknown user id is used, it shows that the record exists
 
     fetch(url,{headers})
